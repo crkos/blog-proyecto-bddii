1d679a82559b1168c571b1218a11503564e54cca
crkos <jordandark123@gmail.com>
2022-12-06 20:22:09 -0700

    Adde URI DB

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const db = require('./config');
 
 const getConnection = async () => {
-    return mysql.createConnection(db);
+    return mysql.createConnection("mysql://evcwzmb8o4fpxega0au6:pscale_pw_AbDaYkwCzMg4I6Fu9sGzGr11XdPxeVnsaJjMnhnFRHj@us-east.connect.psdb.cloud/apps-moviles?ssl={\"rejectUnauthorized\":true}");
 }
 
 exports.getConnection = getConnection;
